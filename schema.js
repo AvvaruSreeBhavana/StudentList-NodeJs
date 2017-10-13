14100966df1c612d8d834d64ba8f7228f2385bd3
AvvaruSreeBhavana <avvarusreebhavana8@gmail.com>
2017-10-13 21:48:17 +0530

    first commit

---
+++ schema.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var assert = require('assert');
+var Schema = mongoose.Schema;
+var productSchema = new Schema({
+name: {
+    type: String,
+    require: true,
+    unique: true
+},
+description: {
+    type: String,
+    required: true
+}
+}, {timestamps: true
+
+});
+var Products = mongoose.model('students', productSchema);
+module.exports = Products;
